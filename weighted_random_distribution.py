cb358197a86c4e7fb22dfcad7a5b8f152a9189ff
harsh <harsh@ziploan.in>
2017-10-11 02:18:31 +0530

    added check for starting element greater than ending element.

--- weighted_random_distribution.py
+++ weighted_random_distribution.py
@@ -22,28 +22,32 @@ if __name__ == "__main__":
 
     a = args.get('start')
     b = args.get('end')
-    weights = {
-        "L": args.get('lower'), # assuming we have 100 samples , we want 27 out of them to be lower than half
-        "H": args.get('upper')  # and 73 of them to be greater than half. These are hard-coded as per the question.  
-    }
-    total_weights = sum(weights.values())
-    random_number = dict()   # for maintaining count of randomly selected lower or half elements of range. 
-    random_lower_number = [] # for pushing randomly selected lower half elements
-    random_upper_number = [] # for pushing randomly selected upper half elements
-    random_number['L'] = 0   #initial count is set to zero.
-    random_number['H'] = 0   # "L" is for lower half. "H" is for Upper half.
+    # import pdb; pdb.set_trace()
+    if a > b:
+        print "starting element cannot be greater than ending element."
+    else:
+        weights = {
+            "L": args.get('lower'), # assuming we have 100 samples , we want 27 out of them to be lower than half
+            "H": args.get('upper')  # and 73 of them to be greater than half. These are hard-coded as per the question.  
+        }
+        total_weights = sum(weights.values())
+        random_number = dict()   # for maintaining count of randomly selected lower or half elements of range. 
+        random_lower_number = [] # for pushing randomly selected lower half elements
+        random_upper_number = [] # for pushing randomly selected upper half elements
+        random_number['L'] = 0   #initial count is set to zero.
+        random_number['H'] = 0   # "L" is for lower half. "H" is for Upper half.
 
-    for i in xrange(args.get('total')):
-        random_index = rand_int(0,total_weights-1)
-        if random_index < weights['L']:
-            random_lower_number.append(rand_int(a,(b+a)/2))
-            random_number['L'] += 1 
-        else:
-            random_upper_number.append(rand_int((a+b+2)/2,b))
-            random_number['H'] += 1
-    print "low"
-    print random_lower_number
-    print "high"
-    print random_upper_number
-    print "count"
-    print random_number
+        for i in xrange(args.get('total')):
+            random_index = rand_int(0,total_weights-1)
+            if random_index < weights['L']:
+                random_lower_number.append(rand_int(a,(b+a)/2))
+                random_number['L'] += 1 
+            else:
+                random_upper_number.append(rand_int((a+b+2)/2,b))
+                random_number['H'] += 1
+        print "low"
+        print random_lower_number
+        print "high"
+        print random_upper_number
+        print "count"
+        print random_number
